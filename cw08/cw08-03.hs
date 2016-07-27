8b28261e87558fd8ae4039c2a950e50db548f1ec
Marcin Kacprzak <martin.kacprzak@gmail.com>
2016-07-27 10:55:37 +0200

    cw08-03

---
+++ cw08/cw08-03.hs
@@ -0,0 +1,34 @@
+-- cw08-03
+import Text.Printf
+import System.IO
+import System.Environment
+
+qEquation :: Double -> Double -> Double -> (Double, Double)
+qEquation a b c = (x1, x2)
+  where
+    x1 = e + sqrt d / (2 * a)
+    x2 = e - sqrt d / (2 * a)
+    d = b * b - 4 * a * c
+    e = - b / (2 * a)
+
+qEquationResultToString :: (Double, Double) -> String
+qEquationResultToString (a, b)
+  | isNaN(a) && isNaN(b) = "Równanie nie posiada rozwiązań"
+  | a == b = printf "Równanie ma jedno rozwiązanie:\n x = %.2f\n" a
+  | otherwise = printf "Obliczone pierwiastki to:\n x1 = %.2f, x2 = %.2f\n" a b
+
+qEquationUI :: Double -> Double -> Double -> IO ()
+qEquationUI 0 _ _ = putStrLn "To nie jest równanie kwadratowe!"
+qEquationUI a b c = do
+  putStrLn "Dla równania kwadratowego o współczynnikach:"
+  printf " a = %.2f, b = %.2f i c = %.2f\n" a b c
+  putStrLn . qEquationResultToString $ qEquation a b c
+
+main = do putStrLn "Program obliczający pierwiastki równania kwadratowego."
+          argv <- getArgs
+          if not (null argv) && (length argv) == 3
+            then let a = read (head argv)
+                     b = read (argv !! 1)
+                     c = read (argv !! 2)
+                 in qEquationUI a b c
+            else putStrLn "Sposób użycia: cw08-03 a b c"
