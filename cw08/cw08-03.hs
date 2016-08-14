fdb16e72ef0e7cce8d9fa54b97f82711289bdf4b
Marcin Kacprzak <martin.kacprzak@gmail.com>
2016-08-14 16:41:17 +0200

    qEquation monad version

--- cw08/cw08-03.hs
+++ cw08/cw08-03.hs
@@ -3,19 +3,21 @@ import Text.Printf
 import System.IO
 import System.Environment
 
-qEquation :: Double -> Double -> Double -> (Double, Double)
-qEquation a b c = (x1, x2)
+qEquation :: Double -> Double -> Double -> [Double]
+qEquation a b c
+  | d < 0 = []
+  | d == 0 = [e]
+  | d > 0 = [x1, x2]
   where
     x1 = e + sqrt d / (2 * a)
     x2 = e - sqrt d / (2 * a)
     d = b * b - 4 * a * c
     e = - b / (2 * a)
 
-qEquationResultToString :: (Double, Double) -> String
-qEquationResultToString (a, b)
-  | isNaN(a) && isNaN(b) = "Równanie nie posiada rozwiązań"
-  | a == b = printf "Równanie ma jedno rozwiązanie:\n x = %.2f" a
-  | otherwise = printf "Obliczone pierwiastki to:\n x1 = %.2f, x2 = %.2f" a b
+qEquationResultToString :: [Double] -> String
+qEquationResultToString [] = "Równanie nie posiada rozwiązań"
+qEquationResultToString (x:[]) = printf "Równanie ma jedno rozwiązanie:\n x = %.2f" x
+qEquationResultToString (x1:x2:_) = printf "Obliczone pierwiastki to:\n x1 = %.2f, x2 = %.2f" x1 x2
 
 qEquationUI :: Double -> Double -> Double -> IO ()
 qEquationUI 0 _ _ = putStrLn "To nie jest równanie kwadratowe!"
